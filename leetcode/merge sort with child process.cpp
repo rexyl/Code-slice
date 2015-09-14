bd0838a3195b1c54f45b94a4f7fef9daa58d1f2a
Lu Yang <Rex@Lus-MacBook-Pro.local>
2015-09-14 16:10:32 -0400

    modify of merge sort thread version (ref)

--- leetcode/merge sort with child process.cpp
+++ leetcode/merge sort with child process.cpp
@@ -18,18 +18,18 @@ void merge_sort(vector<T>& array) {
     }
 }
 
-//void merge_sort_thread(vector<int>& array) {
-//    if (1 < array.size()) {
-//        vector<int> array1(array.begin(), array.begin() + array.size() / 2);
-//        thread first(merge_sort_thread,array1);
-//        
-//        vector<int> array2(array.begin() + array.size() / 2, array.end());
-//        thread second(merge_sort_thread,array2);
-//        first.join(); //wait for both ended
-//        second.join();
-//        merge (array1.begin(),array1.end(),array2.begin(),array2.end(),array.begin());
-//    }
-//}
+void merge_sort_thread(vector<int>& array) {
+    if (1 < array.size()) {
+        vector<int> array1(array.begin(), array.begin() + array.size() / 2);
+        thread first(merge_sort_thread,ref(array1));
+        
+        vector<int> array2(array.begin() + array.size() / 2, array.end());
+        thread second(merge_sort_thread,ref(array2));
+        first.join(); //wait for both ended
+        second.join();
+        merge (array1.begin(),array1.end(),array2.begin(),array2.end(),array.begin());
+    }
+}
 
 //template <typename T>
 //void merge_sort_thread_arbi(vector<T>& array,int i) {
@@ -44,20 +44,20 @@ void merge_sort(vector<T>& array) {
 //    }
 //}
 
-int main()
-{
-    clock_t timer1,timer2;
-    timer1 = clock();
-    int a[] = {2,1,3,4,5,7,9,1,3,3,3};
-    vector<int> v1(&a[0],&a[sizeof(a)/4]);
-    merge_sort(v1);
-    timer2 = clock();
-    cout<<double(timer2-timer1)/CLOCKS_PER_SEC;
-    string s(" United  state of America");
-    vector<string> strs;
-    boost::split(strs, s, boost::is_any_of( " " ));
-    for (int i=0; i<strs.size(); i++) {
-        cout<<strs[i]<<endl;
-    }
-    return 0;
-}
+//int main()
+//{
+//    clock_t timer1,timer2;
+//    timer1 = clock();
+//    int a[] = {2,1,3,4,5,7,9,1,3,3,3};
+//    vector<int> v1(&a[0],&a[sizeof(a)/4]);
+//    merge_sort_thread(v1);
+//    timer2 = clock();
+//    cout<<double(timer2-timer1)/CLOCKS_PER_SEC;
+////    string s(" United  state of America");
+////    vector<string> strs;
+////    boost::split(strs, s, boost::is_any_of( " " ));
+////    for (int i=0; i<strs.size(); i++) {
+////        cout<<strs[i]<<endl;
+////    }
+////    return 0;
+//}
