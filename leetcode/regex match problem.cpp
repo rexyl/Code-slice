e7c2b38c60590d8b8bdef2c50b6f5329b1317fa0
Lu Yang <Rex@Lus-MacBook-Pro.local>
2015-09-14 23:10:05 -0400

    regex

--- leetcode/regex match problem.cpp
+++ leetcode/regex match problem.cpp
@@ -6,27 +6,28 @@
 #include <iostream>
 using namespace std;
 
-//class Solution {
-//    public: boolean isMatch(string s, string p) {
-//        if (p.length() == 0) {
-//            return s.length() == 0;
-//        }
-//        if (p.length() == 1) {
-//            return s.length() == 1
-//            && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0)) ;
-//        }
-//        // p.length() i bigger than 1 here
-//        if (p.charAt(1) == '*') {
-//            if (isMatch(s, p.substring(2))) {
-//                return true;
-//            }
-//            return s.length() > 0
-//            && (p.charAt(0) == '.' || s.charAt(0) == p.charAt(0))
-//            && isMatch(s.substring(1), p);
-//        } else {
-//            return s.length() > 0
-//            && (p.charAt(0) == '.' || s.charAt(0) == p.charAt(0))
-//            && isMatch(s.substring(1), p.substring(1));
-//        }
-//    }
-//}
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if (p.length() == 0) {
+            return s.length() == 0;
+        }
+        if (p.length() == 1) {
+            return s.length() == 1
+            && (p[0] == '.' || p[0] == s[0]) ;
+        }
+        // p.length() i bigger than 1 here
+        if (p[1] == '*') {
+            if (isMatch(s, p.substr(2))) {
+                return true;
+            }
+            return s.length() > 0
+            && (p[0] == '.' || s[0] == p[0])
+            && isMatch(s.substr(1), p);
+        } else {
+            return s.length() > 0
+            && (p[0] == '.' || s[0] == p[0])
+            && isMatch(s.substr(1), p.substr(1));
+        }
+    }
+};
